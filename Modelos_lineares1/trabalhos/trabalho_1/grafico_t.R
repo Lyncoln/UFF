a233583e28baf646bc8b64689a67b99494ad72d7
Daniel dos Santos <dd_santos@id.uff.br>
2019-09-14 16:26:43 -0300

    creates graph for T distribuition

---
+++ Modelos_lineares1/trabalhos/trabalho_1/grafico_t.R
@@ -0,0 +1,68 @@
+bd = foreign::read.spss("internacoes.sav")
+bd = as.data.frame(bd)
+
+degree = 98
+quantile = qt(0.975, df = degree)
+rc_values = seq(-4, -quantile, length = 100)
+denisty_rc_values = dt(rc_values, df = 46)
+ic_values = seq(-quantile, quantile, length = 100)
+denisty_ic_values = dt(ic_values, df = 46)
+
+plot(
+  function(x)
+    dt(x, df = 46),
+  xlim = c(-4, 4),
+  ylab = '',
+  xlab = 'Quantis',
+  bty="n",
+  yaxt='n',
+  xaxt='n'
+)
+axis(side=1, at=round(c(-4, -quantile, 0 ,quantile,4), 2))
+
+polygon(
+  x = c(-4, rc_values, -quantile),
+  y = c(0, denisty_rc_values, 0),
+  border = FALSE,
+  col = 'red',
+  density = 50
+)
+polygon(
+  x = c(quantile, sort(-1 * rc_values), 4),
+  y = c(0, sort(denisty_rc_values, decreasing = TRUE), 0),
+  border = FALSE,
+  col = 'red',
+  density = 50
+)
+polygon(
+  x = c(-quantile, ic_values, quantile),
+  y = c(0, denisty_ic_values, 0),
+  border = FALSE,
+  col = '#94eaf7',
+  density = 50
+)
+
+lines(
+  x = c(-quantile, -quantile),
+  y = c(0, denisty_rc_values[length(denisty_rc_values)]),
+  lty = 2
+)
+lines(
+  x = c(quantile, quantile),
+  y = c(0, denisty_rc_values[length(denisty_rc_values)]),
+  lty = 2
+)
+
+lines(x=c(-4,4), y=c(0,0))
+
+text(-3, 0.07, expression(frac(alpha, 2)), cex=1.3)
+text(3, 0.07, expression(frac(alpha,2)), cex=1.3)
+text(0, 0.15, expression(1 - alpha), cex=1.3)
+
+par(xpd=TRUE)
+text(2.3, -0.094, expression(alpha == 0.05))
+legend(3,0.35,legend = c('IC', 'RC'), 
+       fill = c('#94eaf7', 'red'), 
+       density = 50)
+
+par(xpd=FALSE)
